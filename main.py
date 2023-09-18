407b95c4e8e5c476eb7a355fdb5692b73391d3d4
Ethan Cloin <psnethan@gmail.com>
2023-09-17 22:42:18 -0400

    Hard reset to script style v1: Add pseudocode for main flow

--- main.py
+++ main.py
@@ -1,188 +1,79 @@
-import logging
-from pprint import pprint
-from debt import Debt
-from net_worth import NetWorth
-from cash_flow import CashFlow, MonthlyCashFlow
-
-
-logging.basicConfig(level=logging.DEBUG)
-
-
-def test_debt():
-    from debt import Debt
-
-    # student_loan = Debt("Student Loan", 650000, 0.0275, 10)
-    # student_loan = Debt("Student Loan", 2000000, 0.05, 10)
-
-    car_loan = Debt("Auto Loan", 1400000, 0.05, 3)
-    for _ in range(0, 125):
-        try:
-            car_loan.make_min_monthly_payment()
-        except Exception as err:
-            logging.warning(err)
-            pass
-    print(car_loan)
+"""
+script form of financial forecaster.
+to force simplicity, do not utilize any OOP
+"""
 
 
 def main():
-    net_worth = initialize_net_worth()
-    cash_flow = initialize_monthly_cashflow()
+    GOAL_DOLLARS = 100000
+    net_worth = get_net_worth()
+    income = get_income()
+    spending = get_spending(net_worth["debts"], income["net_monthly"])
+    action_plan = build_action_plan(income, spending, net_worth)
 
-    after_a_month = simulate_month(net_worth=net_worth, cash_flow=cash_flow)
-    pprint(after_a_month)
+    months = 0
+    while net_worth["total_dollars"] < GOAL_DOLLARS:
+        new_net_worth = execute_action_plan(net_worth, action_plan)
+        net_worth = new_net_worth
+        months += 1
 
-
-def apply_passive_growth(net_worth: NetWorth) -> NetWorth:
-    net_worth.accrue_savings_interest()
-    net_worth.grow_investments()
-    return net_worth
-
-
-def contribute_to_retirement(
-    net_worth: NetWorth, cash_flow: CashFlow, current_monthly_income: int
-) -> tuple[NetWorth, int]:
-    ira_retirement = cash_flow.get_monthly_roth_contribution()
-    job_retirement = cash_flow.get_monthly_precheck_contribution()
-    net_worth.contribute_to_401k(job_retirement)
-    net_worth.contribute_to_roth_ira(ira_retirement)
-
-    current_monthly_income -= ira_retirement
-
-    return net_worth, current_monthly_income
-
-
-def save_spend_invest(
-    net_worth: NetWorth, cash_flow: CashFlow, current_month_income: int
-) -> tuple[NetWorth, int]:
-    net_worth.deposit_to_savings(cash_flow.longterm_savings)
-    current_month_income -= cash_flow.longterm_savings
-
-    net_worth.deposit_to_brokerage(cash_flow.investment_brokerage)
-    current_month_income -= cash_flow.investment_brokerage
-
-    net_worth.submit_debt_payment_to_total(cash_flow.debt_payments)
-    current_month_income -= cash_flow.debt_payments
-
-    current_month_income -= cash_flow.expenses
-    current_month_income -= cash_flow.shortterm_savings
-
-    return net_worth, current_month_income
-
-
-def initialize_net_worth() -> NetWorth:
-    cash = {
-        "savings": 9000,
-        "checking": 3200,
-    }
-
-    retirement = {
-        "401k": 2500,
-        "Traditional IRA": 1900,
-        "Roth IRA": 19100,
-    }
-
-    brokerage = 300
-    crypto = 1500
-
-    debt = {
-        "student": 6500,
-        "auto": 14000,
-    }
-
-    return NetWorth(
-        cash=cash, retirement=retirement, crypto=crypto, brokerage=brokerage, debts=debt
-    )
-
-
-def initialize_monthly_cashflow() -> CashFlow:
-    paycheck = 2500
-    retirement_contribution = 410
-    roth_contribution = 250
-
-    monthly_expenses = 3150
-    car_payment = 250
-    student_loan = 100
-
-    # according to rocketmoney budget, i typically have 650
-    # to play with after roth, retirement, and expenses for a paycheck
-    investments = 400
-    savings = 250  # since emergency fund is filled, this goes to purchases or whateva
-
-    return CashFlow(
-        paycheck=paycheck,
-        precheck_contribution=retirement_contribution,
-        roth_ira_contribution=roth_contribution,
-        expenses=monthly_expenses,
-        investment_brokerage=investments,
-        shortterm_savings=savings,
-        debt_payments=car_payment + student_loan,
-    )
-
-
-def simulate_month(net_worth: NetWorth, cash_flow: CashFlow) -> NetWorth:
-    """
-    return updated net worth object for a typical month
-
-    accrue interest on cash_savings, apply growth to investments, apply payments to debts.
-    """
-    old_net_worth = net_worth.shallow_clone()
-    pprint(old_net_worth)
-
-    # passive growth
-    current_monthly_income = cash_flow.get_monthly_paycheck()
-    net_worth = apply_passive_growth(net_worth, cash_flow)
-
-    # retirement
-    net_worth, current_monthly_income = contribute_to_retirement(
-        net_worth, cash_flow, current_monthly_income
-    )
-
-    # saving, investing, spending
-    net_worth, current_monthly_income = save_spend_invest(
-        net_worth, cash_flow, current_monthly_income
-    )
-
-    logging.info(
-        f"Paycheck remaining after all spending and saving: {current_monthly_income}. Depositing in checking"
-    )
-    net_worth.deposit_to_checking(current_monthly_income)
-    print(f"updated net worth: {net_worth.get_total()}")
-    return net_worth
-
-
-def simulate_month2(net_worth: NetWorth, cash_flow: MonthlyCashFlow):
     pass
 
 
-def get_user_net_worth() -> NetWorth:
-    return NetWorth(
-        cash={
-            "checking": 240000,
-            "saving": 500000,
-        },
-        retirement={
-            "IRA": 2000000,
-            "401k": 2500000,
-        },
-        brokerage=0,
-        crypto=0,
-        debts=[
-            Debt(
-                title="Auto Loan",
-                principal=3000000,
-                APR=0.06,
-                term_years=4,
-            ),
-            Debt(
-                title="Mortgage",
-                principal=45000000,
-                APR=0.04,
-                term_years=30,
-            ),
-        ],
-    )
+def get_net_worth():
+    # Banking
+    # Retirement
+    # Investment
+    # Debt
+    pass
+
+
+def get_income():
+    # Gross Annual
+    # Retirement Contribution
+    # Net Monthly
+    pass
+
+
+def get_spending(debts, net_monthly_income):
+    # Minimum Debt Payments
+    # Housing
+    # Groceries
+    # Frivolous Spending (dining, subscriptions, etc)
+    # Available for Saving (net monthly - above)
+    pass
+
+
+def build_action_plan(net_monthly_income, monthly_spending, net_worth):
+    """
+    is there available income ? prioritize : exception
+        is there high-interest-debt ? pay to it
+            is there remaining available income ? return that amt w status
+        is there remaining available income ? contribute to roth ira :
+            is there remaining available income ? return that amt w status
+        is there remaining available income ? contribute to 401k
+            is there remaining available income ? return that amt w status
+        is there remaining available income ? contribute to brokerage
+
+    action_plan: {
+        available_income: int,
+        high_interest_debt_payments: int,
+        roth_ira_contribution: int,
+        401k_contribution: int,
+        brokerage_contribution: int,
+    }
+    """
+    pass
+
+
+def execute_action_plan(net_worth, action_plan):
+    """
+    returned an updated net worth after applying contributions to decrease
+    debts and increase investments
+    """
+    pass
 
 
 if __name__ == "__main__":
-    # main()
-    test_debt()
+    print("Hello World!")
+    main()
