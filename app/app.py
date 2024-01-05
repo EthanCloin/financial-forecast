62bb82251173322659f9966465e1c7175007ce59
Ethan Cloin <psnethan@gmail.com>
2024-01-04 22:17:29 -0500

    Enable hot reloading during development

--- app/app.py
+++ app/app.py
@@ -24,4 +24,11 @@ app = get_app
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000, factory=True)
+    uvicorn.run(
+        "app:app",
+        host="127.0.0.1",
+        port=8000,
+        factory=True,
+        reload=True,
+        reload_includes="*.html",
+    )
